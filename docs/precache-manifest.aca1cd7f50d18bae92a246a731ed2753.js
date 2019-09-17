9eac507d973c113c96adb701952213dc8c89c6b3
Mar Prieto <mar.pnieto@gmail.com>
2019-09-17 12:00:22 +0200

    New homepage

---
+++ docs/precache-manifest.aca1cd7f50d18bae92a246a731ed2753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3261914d3145182a2942d91b025e9482",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c809ee3afcceb4d07658",
+    "url": "./static/css/main.2fb3cc26.chunk.css"
+  },
+  {
+    "revision": "b12d1815549d1e391b88",
+    "url": "./static/js/2.dc4b42c6.chunk.js"
+  },
+  {
+    "revision": "c809ee3afcceb4d07658",
+    "url": "./static/js/main.b037e6be.chunk.js"
+  },
+  {
+    "revision": "2d97c7532914cad2d668",
+    "url": "./static/js/runtime~main.00f5acd8.js"
+  }
+]);
