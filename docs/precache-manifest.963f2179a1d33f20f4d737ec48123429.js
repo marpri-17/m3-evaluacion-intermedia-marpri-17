6b93f53c688878ef42c0ba6e240b85e8e18bca88
Mar Prieto <mar.pnieto@gmail.com>
2019-09-17 11:42:13 +0200

    Clean task

---
+++ docs/precache-manifest.963f2179a1d33f20f4d737ec48123429.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb20ef2df2320a849cac255b2ed09c5",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "c809ee3afcceb4d07658",
+    "url": "/myapp/static/css/main.2fb3cc26.chunk.css"
+  },
+  {
+    "revision": "b12d1815549d1e391b88",
+    "url": "/myapp/static/js/2.dc4b42c6.chunk.js"
+  },
+  {
+    "revision": "c809ee3afcceb4d07658",
+    "url": "/myapp/static/js/main.b037e6be.chunk.js"
+  },
+  {
+    "revision": "aa790f966f25ebe514f5",
+    "url": "/myapp/static/js/runtime~main.8b518b0c.js"
+  }
+]);
